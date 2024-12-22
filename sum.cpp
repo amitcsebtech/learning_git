130ffdde3c8e7d172a569ced9ee890c759fe0159
amitcsebtech <amitsingh123genex@gmail.com>
2024-12-23 02:17:55 +0530

    Sum of two number

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"Enter number 1\n";
+    cin>>num1;
+    cout<<"Enter number 2\n";
+    cin>>num2;
+    cout<<"Sum of number="<<num1+num2;
+    return 0;
+}
